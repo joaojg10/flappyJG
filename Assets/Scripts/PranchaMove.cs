0a4c0335e1a459989ec482c6336374c63532fb7f
Joao Guilherme <joaoguilhermeos1@gmail.com>
2017-02-21 12:42:25 -0300

    Video 6

---
+++ Assets/Scripts/PranchaMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PranchaMove : MonoBehaviour
+{
+
+    public float velocidade = 1f;
+    public float limite;
+    public float retorno;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 velocidadeVetorial = Vector3.left;
+
+        transform.position = transform.position + velocidadeVetorial * Time.deltaTime;
+
+        if (transform.position.x <= limite)
+        {
+            transform.position = new Vector3(retorno, transform.position.y, transform.position.z);
+        }
+
+    }
+}
